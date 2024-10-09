94078b6410e6fb5d586f05da50c60e0b1833c97a
Daniela Mendoza <inadmendoza1996@gmail.com>
2024-10-08 23:27:36 -0500

    tarea-1 Dom

---
+++ Dom-1/data.js
@@ -0,0 +1,10 @@
+let divId = document.getElementById("contenedor");
+let boton = document.getElementById("boton");
+
+boton.getElementById("click", texto)
+
+function texto() {
+  divId.innerHTML = "esto lo agrego el primer div";
+  divId.style.margin = "50px";
+  divId.style.background = "gray";
+}
